6c46eb2ae1d0123bd9e2d75ae660af97d4a1e2d0
trainerx12345 <xdsamuelcorpuz@gmail.com>
2023-04-02 14:53:56 +0800

    test commit

---
+++ src/library/Logging.ts
@@ -0,0 +1,8 @@
+export default class Logging {
+  public static log = (args: any) => this.info(args)
+    public static info = (args: any) => console.log('\x1b[36m%s\x1b[0m', `${new Date().toLocaleString()} [INFO]`, typeof args === 'string' ? args : args);
+
+    public static warn = (args: any) => console.log('\x1b[33m%s\x1b[0m', `${new Date().toLocaleString()} [INFO]`, typeof args === 'string' ? args : args);
+
+    public static error = (args: any) => console.log('\x1b[31m', `${new Date().toLocaleString()} [INFO]`, typeof args === 'string' ? args : args);
+}
